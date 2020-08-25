388fbb7f7b4c9ceabf075fec367d2de6bfc11e4f
agronselimi1997 <45064623+agronselimi1997@users.noreply.github.com>
2020-08-25 19:52:31 +0200

    Added Control Flow Grapgh Letters on the function Added SILab2Test.java

--- src/main/java/SILab2.java
+++ src/main/java/SILab2.java
@@ -1,19 +1,29 @@
 public class SILab2 {
 
+
         static boolean checkPrime(int n) {
-            if (n < 0) {
-                throw new RuntimeException("Number should not be negative");
+
+            if (n < 0)//A
+                 {
+                throw new RuntimeException("Number should not be negative");//B
             }
             int m = n / 2;
-            if (n == 0 || n == 1) {
-                return false;
-            } else {
-                for (int i = 2; i <= m; i++) {
-                    if (n % i == 0) {
-                        return true;
+            if (n == 0 || n == 1)//C
+                 {
+                return false;//D
+            } else //E
+                 {
+                for (int i = 2;//F
+                     i <= m;//G
+                     i++)//H
+                     {
+                    if (n % i == 0)//I
+                         {
+                        return true;//J
                     }
+
                 }
-                return false;
+                return false;//K
             }
         }
 
---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,7 @@
+import org.junit.Test;
+
+public class SILab2Test {
+    @Test
+    public void checkPrime() {
+    }
+}
